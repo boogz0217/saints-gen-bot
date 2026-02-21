5eaa0cdf05d1e667e3d6a28dd95c563f2dac93a9
boogz0217 <boogz0217@users.noreply.github.com>
2026-02-20 20:43:50 -0500

    Bump version to 2.6.0

--- api.py
+++ api.py
@@ -71,8 +71,8 @@ def parse_version(version: str) -> tuple:
 # Per-product version requirements (used for license version enforcement)
 PRODUCT_VERSIONS = {
     "saints-gen": {
-        "current": "2.5.9",
-        "min": "2.5.9",
+        "current": "2.6.0",
+        "min": "2.6.0",
         "message": "Please download the latest version from the Discord server."
     },
     "saints-shot": {
@@ -83,8 +83,8 @@ PRODUCT_VERSIONS = {
 }
 
 # Default/legacy versions (for clients that don't specify product)
-CURRENT_VERSION = "2.5.9"
-MIN_VERSION = "2.5.9"
+CURRENT_VERSION = "2.6.0"
+MIN_VERSION = "2.6.0"
 
 
 def check_version_allowed(product: str, version: str) -> tuple:
